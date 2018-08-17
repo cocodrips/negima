c3d7c50d1e00be6568c6ce5b1fea71a9833611c2
cocodrips <cocodrips@gmail.com>
2018-08-17 16:27:31 +0900

    Add tsv/xlsx sample.

--- sample.py
+++ sample.py
@@ -1,7 +1,11 @@
 from negima import MorphemeMerger
 mm = MorphemeMerger()
+# csv
 mm.set_rule_from_csv('rules/1_noun.csv')
+# tsv
+# mm.set_rule_from_csv('rules/1_noun.tsv', sep='\t')
+# # excel
+# mm.set_rule_from_excel('rules/rules.xlsx', sheet_name='1_noun')
 
 words, posses = mm.get_rule_pattern('今日はいい天気')
 print(words)
-
